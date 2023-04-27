a11edc4ec9aa60f80bb0a6a3b81347cb8190b4bd
Joey Wang <joeywdz@gmail.com>
2023-04-27 22:30:56 +0800

    feat: add action fallback

--- lib/scan_web/controllers/account_controller.ex
+++ lib/scan_web/controllers/account_controller.ex
@@ -3,6 +3,9 @@ defmodule ScanWeb.AccountController do
 
   alias Scan.Accounts
 
+  # 如果发生conn错误也就是说没有找到对应的action，那么就会调用FallbackController的call方法
+  action_fallback ScanWeb.FallbackController
+
   ## 注册账户
   def signup(conn, %{"user" => params}) do
     with {:ok, user} <- Accounts.create(params) do
